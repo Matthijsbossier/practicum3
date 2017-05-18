8a959bc07236f1aa767b9175e731a39de8920ff6
svber <srjberni@avans.nl>
2017-05-18 23:38:08 +0200

    begonnen aan search

--- programeren-4-practicum-3/practicum 3/routes/country.js
+++ programeren-4-practicum-3/practicum 3/routes/country.js
@@ -14,35 +14,35 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.get('/info', function(request, response) {
- response.status(200);
- response.json({
- "description": "Recipes"
- });
+	response.status(200);
+ 	response.json({
+ 	"description": "Recipes"
+ 	});
 });
 
 router.get('/countries', function(request, response){
 	response.status(200);
 	connection.query('SELECT * from country', function(error, rows, fields) {
-			if (error)
-				console.log('' + error);
-			else
-				console.log("gelukt");
-				response.json({rows});
-		});	
+		if (error)
+			console.log('' + error);
+		else
+			console.log("gelukt");
+			response.json({rows});
+	});	
 });
 
 router.get('/countries/:id', function(request, response){
 	response.status(200);
 	connection.query('SELECT * from country', function(error, rows, fields) {
-			if (error)
-				console.log('' + error);
-			else
-				console.log("gelukt");
-				
-				var id = request.params.id || '';
-				var country = rows[id];
-				response.json({country});
-		});
+		if (error)
+			console.log('' + error);
+		else
+			console.log("gelukt");
+			
+			var id = request.params.id || '';
+			var country = rows[id];
+			response.json({country});
+	});
 });
 
 router.post('/countries', function(request, response){
@@ -57,8 +57,16 @@ router.delete('/countries/:id', function(request, response){
 
 });
 
-router.get('/search?type=country&continent='+ +'&limit=24', function(request, response){
-
+router.get('/search?type=country&continent=:continent&limit=24', function(request, response){
+	response.status(200);
+	var continent = request.params.continent || '';
+	connection.query('SELECT * from country where continent = '+ continent, function(error, rows, fields) {
+		if (error)
+			console.log('' + error);
+		else
+			console.log("gelukt");
+			response.json({rows});
+	});	
 })
 
 module.exports = router;
